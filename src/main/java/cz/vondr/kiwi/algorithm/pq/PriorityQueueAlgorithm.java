27e19539716604cbf148e425dd55dee5d7ebe621
j.vondrous <j.vondrous@localhost>
2017-03-12 11:09:39 +0100

    Process flights one by one. Not all flights for one day.

--- src/main/java/cz/vondr/kiwi/algorithm/pq/PriorityQueueAlgorithm.java
+++ src/main/java/cz/vondr/kiwi/algorithm/pq/PriorityQueueAlgorithm.java
@@ -23,15 +23,32 @@ public class PriorityQueueAlgorithm implements cz.vondr.kiwi.algorithm.Algorithm
 
     private static final int QUEUE_INITIAL_CAPACITY = 5000;
 
-    private static final Comparator<Progress> comparator = (p1, p2) -> {
+    private final Comparator<Progress> comparator = (p1, p2) -> {
 //        if (p2.path.length < 3 || p1.path.length < 3) {
 //            return p1.path.length - p2.path.length;
 //        }
 
+////        if (bestSolution.price != MAX_VALUE) {
+//            int flightsProcessedDiff = p1.flightsProcessed - p2.flightsProcessed;
+//            if (flightsProcessedDiff != 0) {
+//                return flightsProcessedDiff;
+//            }
+////        }
+
 //        1) delsi cesta
         int pathDiff = p2.path.length - p1.path.length;
+        if (pathDiff != 0) {
+            return pathDiff;
+        }
+
+        int flightsProcessedDiff = p1.flightsProcessed - p2.flightsProcessed;
+        if (flightsProcessedDiff != 0) {
+            return flightsProcessedDiff;
+        }
+
+
 //        2) lepsi cena
-        return pathDiff == 0 ? (p1.price - p2.price) : pathDiff;
+        return p1.price - p2.price;
 
     };
 
@@ -68,8 +85,10 @@ public class PriorityQueueAlgorithm implements cz.vondr.kiwi.algorithm.Algorithm
 
     @Override
     public void start() {
-        //Initial state                       /** bitset 0 = vsechno false */
-        addToQueue(new Progress(new short[0], new BitSet(numberOfCities), 0));
+        //Initial state
+        Progress p1 = new Progress(new short[0], new BitSet(numberOfCities), 0);
+//        p1.flightsProcessed = 1;
+        addToQueue(p1);
 
         Progress p;
         while ((p = queue.poll()) != null) {
@@ -82,45 +101,51 @@ public class PriorityQueueAlgorithm implements cz.vondr.kiwi.algorithm.Algorithm
             Day day = data.days[actualDay];
             City city = day.cities[actualCity];
 
-            for (int actualFlight = 0; actualFlight < city.flights.length; actualFlight++) {
-                Flight flight = city.flights[actualFlight];
 
-                short nextCity = flight.destination;
-
-                //do not flight, where you already been  ||  flight to start, before last day
-                if (p.visitedCities.get(nextCity) || (nextCity == data.startCity && !(actualDay >= numberOfCities - 1))) {
-                    continue;
-                }
-
-                short nextDay = (short) (actualDay + 1);
-                int nextPrice = p.price + flight.price;
-
-                if (actualDay >= numberOfCities - 1) {
-                    if (nextCity == data.startCity) {
-                        if (nextPrice < bestSolution.price) {
-//                            short[] pathCopy = Arrays.copyOf(actualPath, actualPath.length);
-                            bestSolution = new Solution(p.path, nextPrice);
-                            logger.info("New Best solution found. Price = " + nextPrice + ", path=" + Arrays.toString(bestSolution.path) + ", testedFlights=" + testedFlights);
-                        }
-                        //                    logger.info("Solution found. Price = " + nextPrice + ", path=" + Arrays.toString(actualPath) + ", testedFlights="+testedFlights);
-                    }
-                    continue;
-                }
-
-                if (nextPrice >= bestSolution.price) { // very simple throw too expensive paths TODO do it even better
-                    continue;
-                }
-
-                short[] nextPath = new short[p.path.length + 1];
-                System.arraycopy(p.path, 0, nextPath, 0, p.path.length);
-                nextPath[nextPath.length - 1] = nextCity;
-                BitSet nextVisitedCities = (BitSet) p.visitedCities.clone();
-                nextVisitedCities.set(nextCity);
-//                logger.info("Fly from " + actualCity + " to " + nextCity);
-                addToQueue(new Progress(nextPath, nextVisitedCities, nextPrice));
-
-
+            //Remove and insert
+            short actualFlight = p.flightsProcessed;
+            p.flightsProcessed++;
+            if (p.flightsProcessed < city.flights.length) {
+                addToQueue(p);
             }
+
+            //process deeper path
+            Flight flight = city.flights[actualFlight];
+
+            short nextCity = flight.destination;
+
+            //do not flight, where you already been  ||  flight to start, before last day
+            if (p.visitedCities.get(nextCity) || (nextCity == data.startCity && !(actualDay >= numberOfCities - 1))) {
+                continue;
+            }
+
+//            short nextDay = (short) (actualDay + 1);
+            int nextPrice = p.price + flight.price;
+
+            if (actualDay >= numberOfCities - 1) {
+                if (nextCity == data.startCity) {
+                    if (nextPrice < bestSolution.price) {
+//                            short[] pathCopy = Arrays.copyOf(actualPath, actualPath.length);
+                        bestSolution = new Solution(p.path, nextPrice);
+                        logger.info("New Best solution found. Price = " + nextPrice + ", path=" + Arrays.toString(bestSolution.path) + ", testedFlights=" + testedFlights);
+                    }
+                    //                    logger.info("Solution found. Price = " + nextPrice + ", path=" + Arrays.toString(actualPath) + ", testedFlights="+testedFlights);
+                }
+                continue;
+            }
+
+            if (nextPrice >= bestSolution.price) { // very simple throw too expensive paths TODO do it even better
+                continue;
+            }
+
+            short[] nextPath = new short[p.path.length + 1];
+            System.arraycopy(p.path, 0, nextPath, 0, p.path.length);
+            nextPath[nextPath.length - 1] = nextCity;
+            BitSet nextVisitedCities = (BitSet) p.visitedCities.clone();
+            nextVisitedCities.set(nextCity);
+//                logger.info("Fly from " + actualCity + " to " + nextCity);
+            addToQueue(new Progress(nextPath, nextVisitedCities, nextPrice));
+
 
         }
 
