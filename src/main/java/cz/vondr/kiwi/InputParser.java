e583d84c58412ebbeff7db3b61caab636e1092fb
j.vondrous <j.vondrous@localhost>
2017-03-15 23:09:21 +0100

    Parsing info

--- src/main/java/cz/vondr/kiwi/InputParser.java
+++ src/main/java/cz/vondr/kiwi/InputParser.java
@@ -7,23 +7,24 @@ import cz.vondr.kiwi.data.Flight;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 import static cz.vondr.kiwi.Salesman.cityNameMapper;
 import static java.lang.Integer.parseInt;
 import static java.lang.Short.parseShort;
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
 
 public class InputParser {
     private final static Logger logger = LoggerFactory.getLogger(InputParser.class);
 
-    private BufferedReader input;
+    private BufferedInputStream input;
     private Data data;
 
     public InputParser(InputStream input, Data data) {
-        this.input = new BufferedReader(new InputStreamReader(input));
+        this.input = new BufferedInputStream(input);
         this.data = data;
     }
 
@@ -39,19 +40,74 @@ public class InputParser {
 
 //        char[] chars = new char[8024];
 //        while (input.read(chars)!=-1) {
-        while ((line = input.readLine()) != null) {
-            parseLine(line);
+
+
+        // 9.5s - jen parsovani bez vytvareni objektu
+        //19.2s - i s vytvarenim objektu
+        while(readAndParseLine()) {
         }
+
+        // 8.4s - jen parsovani bez vytvareni objektu
+        // 17.3s- i s vytvarenim objektu
+//        while ((line = input.readLine()) != null) {
+//            parseLine(line);
+//        }
         logger.info("Last town was read.");
+
+        //cteni 2s  parsovani 7s   vytvareni objektu 7s  celkem 18s
     }
+
 
     private void readFirstLineWithStartTown() throws Exception {
-        String startTown = input.readLine();
+        input.read(cityBytes);
+
+        String startTown = new String(cityBytes, ISO_8859_1);
         data.startCity = cityNameMapper.nameToIndex(startTown);
+        input.read();//eof
     }
 
-//    int nonsenceStart=0;
-//    int nonsenceEnd=0;
+    private byte[] cityBytes = new byte[3];
+
+    private boolean readAndParseLine() throws IOException {
+
+
+        if (input.read(cityBytes) == -1) {
+            return false; //end of stream
+        }
+
+
+        short from = cityNameMapper.nameToIndex(new String(cityBytes, ISO_8859_1));
+        input.read();//space
+        input.read(cityBytes);
+        short to = cityNameMapper.nameToIndex(new String(cityBytes, ISO_8859_1));
+        input.read();//space
+        int charInt;
+        short dayIndex = 0;
+        while (true) {
+            charInt = input.read();
+            charInt = charInt - 48;
+            if (charInt < 0) {
+                break;
+            }
+            dayIndex = (short) (dayIndex * 10 + charInt);
+        }
+
+        int price = 0;
+        while (true) {
+            charInt = input.read();
+            charInt = charInt - 48;
+            if (charInt < 0) {
+                break;
+            }
+            price = price * 10 + charInt;
+        }
+
+        addFlightToData(from, to, dayIndex, price);
+
+        return charInt != -49;
+
+
+    }
 
     private void parseLine(String line) {
         //TODO do not use regexp
@@ -62,10 +118,12 @@ public class InputParser {
         short to = cityNameMapper.nameToIndex(split[1]);
         short dayIndex = parseShort(split[2]);
         int price = parseInt(split[3]);
+        addFlightToData(from, to, dayIndex, price);
 
 
+    }
 
-
+    private void addFlightToData(short from, short to, short dayIndex, int price) {
         //remove nonsence flight
         if (from == data.startCity && dayIndex != 0) { //lety z pocatecniho mesta. jiny nez prvni (0) den
 //            logger.info("Nonsence Start - "+ ++nonsenceStart);
