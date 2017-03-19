204601be0768fee58e392e7deae57f1a545eaf59
j.vondrous <j.vondrous@localhost>
2017-03-19 19:35:46 +0100

    Backed out changeset: 3252493d7446

--- src/main/java/cz/vondr/kiwi/data/Data.java
+++ src/main/java/cz/vondr/kiwi/data/Data.java
@@ -3,10 +3,7 @@ package cz.vondr.kiwi.data;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Integer.MAX_VALUE;
-
 public class Data {
-    public int minPriceEver = MAX_VALUE;
     public Day[] days;
     public List<Day> daysInput = new ArrayList<>();
 
